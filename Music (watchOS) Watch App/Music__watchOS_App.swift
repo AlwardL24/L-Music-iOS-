e9363acc7ca2ad36d09db586b7079bb332edf2e2
Lucas Alward <lucasdev2008@gmail.com>
2022-11-04 22:05:26 +1300

    Apple Watch Swift UI project generated. Saving in case needed for future use? Unsure

---
+++ Music (watchOS) Watch App/Music__watchOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Music__watchOS_App.swift
+//  Music (watchOS) Watch App
+//
+//  Created by Lucas Alward on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Music__watchOS__Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
